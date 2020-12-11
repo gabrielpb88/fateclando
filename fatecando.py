847a974e4d4375ade2b7bb2a2c2819b86b321c1d
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2020-12-11 13:45:43 -0300

    Corrigido conexão

--- fatecando.py
+++ fatecando.py
@@ -4,44 +4,41 @@ from threading import Thread
 
 with open('private.pem', 'rb') as f:
     privateFile = f.read()
-privateKey = rsa.PrivateKey.load_pkcs1(privateFile, format='PEM')
+pri = rsa.PrivateKey.load_pkcs1(privateFile, format='PEM')
 
 with open('public.pem', 'rb') as f:
   publicKey = f.read()
+pub = rsa.PublicKey.load_pkcs1(publicKey, format='PEM')
+PORT = 20000
 
-# Endereco IP do Servidor
-HOST = '127.0.0.1'
-PORT = 5000
-tcpServidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-orig = (HOST, PORT)
-tcpServidor.bind(orig)
-tcpServidor.listen(1)
-print('####### Servidor rodando #######')
+def conexao():
+    tcpServidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    orig = ('', PORT)
+    tcpServidor.bind(orig)
+    tcpServidor.listen(1)
+    con, ip = tcpServidor.accept()
+    print('####### Servidor rodando #######')
 
-# TODO: Coloque abaixo o endereco IP do Amigo
-SERVER = '192.168.100.75'
-dest = (SERVER, PORT)
-tcpCliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-tcpCliente.connect(dest)
-
-def conexao(con, cli):
     while True:
-        msg = con.recv(1024)
+        msg = con.recv(4096)
         if not msg: break
-        print('\n', cli, rsa.decrypt(msg, privateKey))
-    print('Finalizando conexao do cliente', cli)
+        print('\n', ip, rsa.decrypt(msg, pri))
+    print('Finalizando conexao do cliente', ip)
     con.close()
 
 def cliente():
+    SERVER = input('Digite o IP do destinatario:')
+
+    dest = (SERVER, PORT)
+    tcpCliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcpCliente.connect(dest)
+
     msg = input('Enviar mensagem: ').encode('utf-8')
-    pub = rsa.PublicKey.load_pkcs1(publicKey, format='PEM')
     while msg != '\x18':
         tcpCliente.send(rsa.encrypt(msg, pub))
         msg = input('Enviar mensagem: ').encode('utf-8')
     tcpCliente.close()
 
-while True:
-    con, ip = tcpServidor.accept()
-    print('Conectado por ', ip)
-    Thread(target=conexao, args=(con, ip)).start()
-    Thread(target=cliente).start()
+
+Thread(target=conexao).start()
+Thread(target=cliente).start()
--- gerarChaves.py
+++ gerarChaves.py
@@ -3,7 +3,7 @@ print('Gerador de chaves assimetricas')
 
 (pub, pri) = rsa.newkeys(256)
 
-with open('./public.pem', 'wb') as f:
+with open('public.pem', 'wb') as f:
     f.write(pub.save_pkcs1(format='PEM'))
 
 with open('./private.pem', 'wb') as f:
